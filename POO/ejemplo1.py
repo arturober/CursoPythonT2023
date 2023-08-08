27e8f13e242b6724d05c020498bc368ff5586ed4
Arturo Bernal <arturober@gmail.com>
2023-08-08 21:03:56 +0200

    Clase 8/8

---
+++ POO/ejemplo1.py
@@ -0,0 +1,12 @@
+n = 2
+print(type(n))
+print(n.bit_length()) # <class 'int'>
+
+s = "Hola"
+print(type(s)) # <class 'str'>
+print(s.upper())
+
+def f():
+    pass
+
+print(type(f)) # <class 'function'>
