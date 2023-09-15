debf0bd6579e18a6d8fa5cb0a5db67f530735292
Arturo Bernal <arturober@gmail.com>
2023-09-15 15:54:33 +0200

    productos sqlalchemy

--- SQLAlchemy/1-ejemplo-productos.py
+++ SQLAlchemy/1-ejemplo-productos.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session
-from sqlalchemy import String, Numeric, create_engine
+from sqlalchemy import String, Numeric, create_engine, select
 
 class BaseModel(DeclarativeBase):
     pass
@@ -16,11 +16,49 @@ class Producto(BaseModel):
 
 # Conexión a la base de datos (echo=True -> Modo depuración, imprime las consultas que genera)
 engine = create_engine("sqlite:///SQLAlchemy/productos.sqlite", echo=True) 
-BaseModel.metadata.create_all(engine)
+BaseModel.metadata.create_all(engine) # Crea las tablas si no existen (CREATE TABLE)
 
-silla = Producto(nombre="Silla 2", precio=50.95)
-with Session(engine) as session:
-    session.add(silla)
-    session.commit()
-    print(silla)
+def mostrarProductos():
+    with Session(engine) as session:
+        st = select(Producto) # SELECT * FROM producto
+        productos = session.execute(st).scalars().all()
+        print(productos)
 
+# mostrarProductos()
+
+def mostrarProducto(id: int):
+    with Session(engine) as session:
+        st = select(Producto).where(Producto.id == id) # SELECT * FROM producto WHERE id = X
+        producto = session.execute(st).scalars().one_or_none()
+        print(producto)
+        
+# mostrarProducto(3)
+
+# INSERT
+def insertarProductos():
+    mesa = Producto(nombre="Mesa", precio=100)
+    estanteria = Producto(nombre="Estantería", precio=85.5)
+    armario = Producto(nombre="Armario", precio=234.85)
+    with Session(engine) as session:
+        session.add(mesa)
+        session.add_all([estanteria, armario])
+        session.commit()
+        
+# insertarProductos()
+
+def updateProducto(id: int, nombre: str, precio: float):
+    with Session(engine) as session:
+        producto = session.get(Producto, id) # Equivale a select(Producto).where(Producto.id == id)
+        producto.nombre = nombre
+        producto.precio = precio
+        session.commit()
+        
+# updateProducto(2, "Lámpara", 33)
+
+def deleteProducto(id: int):
+    with Session(engine) as session:
+        producto = session.get(Producto, id)
+        session.delete(producto)
+        session.commit()
+        
+# deleteProducto(3)
