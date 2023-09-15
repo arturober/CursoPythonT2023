616e6aeb0bebb3094128fe4f1724054f99e8046b
Arturo Bernal <arturober@gmail.com>
2023-09-15 18:52:08 +0200

    flask productos

--- Flask/ejemplo1.py
+++ Flask/ejemplo1.py
@@ -1,14 +1,16 @@
+from dataclasses import dataclass
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Numeric
+from sqlalchemy import String, Numeric, select
 
 app = Flask(__name__)
 db = SQLAlchemy()
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///productos.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///productos.sqlite"
 db.init_app(app) # Registramos SQLAlchemy en la aplicación
 
+@dataclass
 class Producto(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     nombre: Mapped[str] = mapped_column(String(255))
@@ -17,8 +19,9 @@ class Producto(db.Model):
 with app.app_context():
     db.create_all() # Creamos las tablas en la BD
 
-@app.get("/hola")
-def hola():
-    return "Hola mundo"
+@app.get("/productos")
+def get_productos():
+    st = select(Producto) # SELECT * FROM producto
+    return db.session.execute(st).scalars().all()
 
 app.run()
