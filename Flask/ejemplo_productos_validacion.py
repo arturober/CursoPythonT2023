a829212dd526b74a808ca32f782d55f4f4c791b7
Arturo Bernal <arturober@gmail.com>
2023-09-22 16:21:22 +0200

    validacion

--- Flask/ejemplo_productos_validacion.py
+++ Flask/ejemplo_productos_validacion.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from marshmallow import Schema,fields, validate
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String, Numeric, select
 
@@ -19,6 +20,29 @@ class Producto(db.Model):
     nombre: Mapped[str] = mapped_column(String(255))
     precio: Mapped[float] = mapped_column(Numeric(10, 2))
 
+class ProductoSchema(Schema): # Validar los datos del producto del cliente
+    nombre = fields.String(required=True, validate=validate.Length(min=4))
+    precio = fields.Number(required=True, validate=validate.Range(min=0))
+
+def validate_json(schema):
+    def decorator(view_func):
+        @wraps(view_func)
+        def wrapper(*args, **kwargs):
+            try:
+                data = request.json
+                schema_instance = schema()
+                validated_data = schema_instance.load(data)
+                request.validated_data = validated_data # Almacena los datos validados
+                return view_func(*args, **kwargs)
+            except ValidationError as error:
+                return jsonify(
+                    {
+                    'error': 'Datos de entrada no válidos',
+                    'messages': error.messages
+                    }
+                ), 400
+        return wrapper
+    return decorator
 
 with app.app_context():
     db.create_all()  # Creamos las tablas en la BD
